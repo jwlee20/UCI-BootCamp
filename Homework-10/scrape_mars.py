269d65ba4667da9e4eb86f2d17308c93286aa5a4
jwlee20 <46329169+jwlee20@users.noreply.github.com>
2019-05-14 19:18:40 -0700

    added hw10 files

---
+++ Homework-10/scrape_mars.py
@@ -0,0 +1,32 @@
+from splinter import Browser
+from bs4 import BeautifulSoup
+
+
+def init_browser():
+    # @NOTE: Replace the path with your actual path to the chromedriver
+    executable_path = {"executable_path": "/usr/local/bin/chromedriver"}
+    return Browser("chrome", **executable_path, headless=False)
+
+
+def scrape():
+    browser = init_browser()
+    
+	url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    browser.visit(url)
+
+    html = browser.html
+    soup = BeautifulSoup(html, "html.parser")
+
+    url_list=[]
+	links = browser.find_by_css("h3")
+
+	for i in range(len(links)):
+	    hemisphere = {}
+	    browser.find_by_css("h3")[i].click()
+	    sample = browser.find_link_by_text('Sample').first
+	    hemisphere['img_url'] = sample['href']
+	    hemisphere['title'] = browser.find_by_css("h2").text
+	    url_list.append(hemisphere)
+	    browser.back()
+
+    return hemisphere
